e1fe29f1c3d09a37a395987be62a185dd78f0b99
Ewan COSTAMAGNA <costamae@tetras.univ-savoie.fr>
2022-04-04 15:17:50 +0200

    oui oui le texte

--- louetacaisse-app/src/App.js
+++ louetacaisse-app/src/App.js
@@ -1,12 +1,15 @@
 import app from "./firebase";
+import "./App.css";
 import { GoogleAuthProvider, getAuth, signInWithRedirect, onAuthStateChanged, signOut } from "firebase/auth";
 import {Container, Col, Row, Button, Navbar, NavbarBrand, NavbarToggler, Collapse, Nav, NavItem, NavLink} from "reactstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useEffect, useState } from 'react';
 import {Route, Routes, Link} from "react-router-dom"
+import { collection, doc, Firestore, getDocs, getFirestore } from "firebase/firestore";
 
 const provider = new GoogleAuthProvider();
 const auth = getAuth(app);
+const db = getFirestore(app);
 
 function Home(){
   return <div>Home</div>
@@ -14,6 +17,96 @@ function Home(){
 function Products(){
   return <div>Products</div>
 }
+
+function CreationVoiture(){
+  return (<div id="div_CreationVoiture" >
+  <table border="1">
+    <tr colspan="2">Ajout d'une voiture dans le catalogue</tr>
+    <br/>
+    <tr
+    >
+      <td>Modèle de la voiture</td>
+      <td><input type="text" id="ModelVoiture"></input></td>
+    </tr>
+    <tr>
+      <td>Type d'essence</td>
+      <td><input type="text" id="EssenceVoiture"></input></td>
+    </tr>
+    <tr>
+      <td>Marque de la voiture</td>
+      <td><input type="text" id="NomVoiture"></input></td>
+    </tr>
+    <tr>
+      <td>Plaque d'immatriculation</td>
+      <td><input type="text" id="Immatriculation"></input></td>
+    </tr>
+    <tr>
+      <td> Prix de vente (€)</td>
+      <td><input type="text" id="PrixVente"></input></td>
+    </tr>
+
+    <button onClick={database}> Enregistrer voiture</button>
+
+  </table>
+
+
+  </div>
+  )
+}
+function database(){
+  
+  var Model_voiture = document.getElementById('ModelVoiture').value;
+  var EssenceVoiture = document.getElementById('EssenceVoiture').value;
+  var NomVoiture = document.getElementById('NomVoiture').value;
+  var Immatriculation = document.getElementById('Immatriculation').value;
+  var PrixVente = document.getElementById('PrixVente').value;
+  
+  console.log('BTN clicked');
+  console.log('Model_voiture = ' + Model_voiture);
+  console.log('EssenceVoiture = ' + EssenceVoiture);
+  console.log('NomVoiture = ' + NomVoiture);
+  console.log('Immatriculation = ' + Immatriculation);
+  console.log('PrixVente = ' + PrixVente);
+
+
+
+}
+
+function Liste_voiture(){
+  //Je fais mes requêtes pour avoir une liste de mes voitures
+
+  const [cars, setCars] = useState([])
+  useEffect(() => {
+    async function getCars(){
+      const querySnapshot = await getDocs(collection(db, "Cars"));
+      querySnapshot.forEach((doc) => {
+        console.log(doc.id, " => ", doc.data());
+
+      });
+      setCars(querySnapshot.docs.map(doc => doc.data()))
+    }
+    getCars()
+  }, [])
+
+  
+  return(
+  <div>
+    <table>
+      <tr>
+        <td>Modèle de la voiture</td>
+        <td>Kilométrage</td>
+        <td>Modèle</td>
+      </tr>
+      <tr>
+        <li>key={doc.Model}</li>
+      </tr>
+    </table>
+  </div>
+ );
+
+} 
+
+
 
 
 function App() {
@@ -32,21 +125,13 @@ useEffect(() => onAuthStateChanged(auth, (newUser) => {
 
   return (
       <Container>
-        <Navbar
-          color="light"
-          expand="md"
-          light>
+        <Navbar color="light" expand="md" light>
         <NavbarBrand href="/">
-        <img
-          width={"30%"}
-          src="loutacaisse.png" />
+          <img width={"30%"} src="loutacaisse.png" />
         </NavbarBrand>
         <NavbarToggler onClick={function noRefCheck() { }}/>
         <Collapse navbar>
-          <Nav
-            className="me-auto"
-            navbar
-          >
+          <Nav className="me-auto" navbar >
             <NavItem>
               <NavLink to="/" tag={Link}>
                 Home
@@ -55,6 +140,16 @@ useEffect(() => onAuthStateChanged(auth, (newUser) => {
             <NavItem>
               <NavLink to="/Products" tag={Link}>
                 Products
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to="/CreationVoiture" tag={Link}>
+              Ajouter une voiture
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to="/Liste_voiture" tag={Link}>
+              Liste des voitures 
               </NavLink>
             </NavItem>
           </Nav>
@@ -66,6 +161,8 @@ useEffect(() => onAuthStateChanged(auth, (newUser) => {
             <Routes>
               <Route path="/" element={<Home />}/>
               <Route path="products" element={<Products />}/>
+              <Route path="CreationVoiture" element={<CreationVoiture />}/>
+              <Route path="Liste_voiture" element={<Liste_voiture />}/>
             </Routes>
           </Col>
         </Row>
