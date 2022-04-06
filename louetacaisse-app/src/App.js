da28fb7f7b1f9292a9af650e8522aa19b3155f0a
lavancha <bouledeneij@gmail.com>
2022-04-06 08:10:31 +0200

    Patch profile security (users can only update himself)

--- louetacaisse-app/src/App.js
+++ louetacaisse-app/src/App.js
@@ -5,7 +5,7 @@ import {Container, Col, Row, Button, Navbar, NavbarBrand, NavbarToggler, Collaps
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useEffect, useState } from 'react';
 import {Route, Routes, Link, useParams} from "react-router-dom"
-import { collection, doc, Firestore, getDocs, getFirestore, setDoc } from "firebase/firestore";
+import { collection, doc, Firestore, getDocs, getFirestore, setDoc, getDoc } from "firebase/firestore";
 
 
 const provider = new GoogleAuthProvider();
@@ -14,70 +14,113 @@ const db = getFirestore(app);
 
 
 
-function Profil(){
-  const [Profils, setProfils] = useState([])
+function Profil(props){
+  console.log("props = " ,props)
+  const [Profil, setProfils] = useState({})
   useEffect(() => {
     async function getProfil(){
-      const querySnapshot = await getDocs(collection(db, "Users"));
-      querySnapshot.forEach((user) => {
-        // doc.data() is never undefined for query doc snapshots
-        // console.log(car.id, " => ", car.data());
-      });
-      setProfils(querySnapshot.docs.map(user => user.data()))
+      if(! props.user) {
+        return 
+      }
+      const docRef = doc(db, "Users", props.user.uid);
+      console.log("hello")
+      const querySnapshot = await getDoc(docRef);
+      console.log("query = ")
+      console.log(querySnapshot.data())
+      setProfils(querySnapshot.data())
     }
     getProfil()
-  }, [])
+  }, [props.user])
 
   return <table id="userTable">
-  <thead>
-      <td>Name</td>
-      <td>First Name</td>
-      <td>Birth Date</td>
-      <td>Adress</td>
-      <td>Phone Number</td>
-      <td>Subscription Date</td>
-      <td>Edit</td>
-  </thead>
-  <tbody>
-    {Profils.map((user) => {
-      // console.log(car);
-      return (
-        <tr>
-          <td>
-            <li>{user.Name}</li>
-          </td>
-          <td>
-            <li>{user.FirstName}</li>
-          </td>
-          {/* <td>
-            <li>{user.BirthDate}</li>
-          </td> */}
-          <td>
+    <tbody>
+      <tr>
+        <td>UID : </td>
+        <td>{props.user?.uid ?? ''}</td>
+      </tr>
+      <tr>
+        <td>Name : </td>
+        <td>{Profil.Name ?? ''}</td>
+      </tr>
+      <tr>
+        <td>First Name : </td>
+        <td>{Profil.Firstname ?? ''}</td>
+      </tr>
+      <tr>
+        <td>Phone Number : </td>
+        <td>{Profil.Phonenumber ?? ''}</td>
+      </tr>
+      <tr>
+        <td>Email : </td>
+        <td>{props.user?.email ?? ''}</td>
+      </tr>
+      <tr>
+        <td>Licence Number : </td>
+        <td>{Profil.Licencenumber ?? ''}</td>
+      </tr>
+    </tbody>
+    <Button><Link to={`/Update/${props.user?.uid}`}>UPDATE</Link></Button>
 
-          </td>
-          <td>
-            <li>{user.Adress}</li>
-          </td>
-          <td>
-            <li>{user.PhoneNumber}</li>
-          </td>
-          <td>
-            bonjour
-          </td>
-          <td>
-            <Button>Edit</Button>
-          </td>
-          {/* <td>
-            <li>{user.SubscriptionDate}</li>
-          </td> */}
-        </tr>
-      )
-      
-    }) }
-    
-  </tbody>
-</table>
+  </table>
+  
 }
+
+
+function EditUser(props){
+
+  const uid = useParams().uid ?? props.user.uid
+  const [name, setName] = useState("")
+  const [firstName, setfirstName] = useState("")
+  const [birthDate, setBirthDate] = useState("")
+  const [Adress, setAdress] = useState("")
+  const [Phone, setPhone] = useState("")
+  const [Licence, setLicence] = useState("")
+
+  async function addUser(user){
+
+    const querySnapshot = await setDoc(doc(db, "Users", uid), {
+      Name: name,
+      Firstname: firstName,
+      Birthdate: birthDate,
+      Adress: Adress,
+      Phonenumber: Phone,
+      Licencenumber: Licence
+    });
+      
+  }
+  return (
+    <table id="tableSetting">
+      <tr>
+      <td>Name :</td>
+      <td><input type="text" id="name" value={name} onChange={e=> setName(e.target.value)} /></td>
+      </tr>
+      <tr>
+      <td>First name :</td>
+      <td><input type="text" id="firstname" value={firstName} onChange={e=> setfirstName(e.target.value)}/></td>
+      </tr>
+      <tr>
+      <td>Birth date :</td>
+      <td><input type="date" id="birthdate"  onChange={e=> setBirthDate(e.target.value)}/></td>
+      </tr>
+      <tr>
+      <td>Adress :</td>
+      <td><input type="text" id="adress" value={Adress} onChange={e=> setAdress(e.target.value)}/></td>
+      </tr>
+      <tr>
+      <td>Phone number :</td>
+      <td><input type="number" id="phonenumber" value={Phone} onChange={e=> setPhone(e.target.value)}/></td>
+      </tr>
+      <tr>
+      <td>Licence number :</td>
+      <td><input type="number" id="licencenumber" value={Licence} onChange={e=> setLicence(e.target.value)}/></td>
+      </tr>
+      <Button><Link to={`/Profil/${props.user.uid}`}>Back</Link></Button>
+      <Button onClick={addUser}>Valider</Button>      
+    </table>
+  )
+}
+
+
 
 
 function Home(){
@@ -108,6 +151,7 @@ function Cars(){
         <td>Fuel</td>
         <td>Price</td>
         <td>Power</td>
+        <td></td>
     </thead>
     <tbody>
       {cars.map((car) => {
@@ -132,6 +176,9 @@ function Cars(){
             <td>
               <li>{car.HP}</li>
             </td>
+            <td>
+              <li><Button>UPDATE</Button></li>
+            </td>
           </tr>
         )
         
@@ -141,66 +188,9 @@ function Cars(){
   </table>
 }
 
-function Settings(props){
-
-  const uid = useParams().uid ?? props.user.uid
-  const [name, setName] = useState("")
-  const [firstName, setfirstName] = useState("")
-  const [birthDate, setBirthDate] = useState("")
-  const [Adress, setAdress] = useState("")
-  const [Phone, setPhone] = useState("")
-  const [Licence, setLicence] = useState("")
-
-  async function addUser(user){
-
-    const querySnapshot = await setDoc(doc(db, "Users", uid), {
-      Name: name,
-      Firstname: firstName,
-      Birthdate: birthDate,
-      Adress: Adress,
-      Phonenumber: Phone,
-      Licencenumber: Licence
-    });
-      
-  }
-  return (
-    <table id="tableSetting">
-      <tr>
-      <td>UID :{uid}</td>
-      <td><input type="text" id="name" value={name} onChange={e=> setName(e.target.value)} /></td>
-      </tr>
-      <tr>
-      <td>Name :</td>
-      <td><input type="text" id="name" value={name} onChange={e=> setName(e.target.value)} /></td>
-      </tr>
-      <tr>
-      <td>First name :</td>
-      <td><input type="text" id="firstname" /></td>
-      </tr>
-      <tr>
-      <td>Birth date :</td>
-      <td><input type="date" id="birthdate" /></td>
-      </tr>
-      <tr>
-      <td>Adress :</td>
-      <td><input type="text" id="adress" /></td>
-      </tr>
-      <tr>
-      <td>Phone number :</td>
-      <td><input type="text" id="phonenumber" /></td>
-      </tr>
-      <tr>
-      <td>Licence number :</td>
-      <td><input type="text" id="licencenumber" /></td>
-      </tr>
-      <Button onClick={addUser}>Valider</Button>      
-    </table>
-  )
-}
-
 
 function App() {
-const [user, setUser] = useState([])
+const [user, setUser] = useState(null)
 
 /******************************** api get firebase ==> plusieurs useEffect possible ? ******************************************/ 
 
@@ -244,7 +234,7 @@ useEffect(() => onAuthStateChanged(auth, (newUser) => {
               </NavLink>
             </NavItem>
             <NavItem>
-              <NavLink to="/Profil" tag={Link}>
+              <NavLink to={`/Profil/${user?.uid}`} tag={Link}>
               Profil
               </NavLink>
             </NavItem>
@@ -264,12 +254,14 @@ useEffect(() => onAuthStateChanged(auth, (newUser) => {
             <Routes>
               <Route path="/" element={<Home />}/>
               <Route path="Voitures" element={<Cars />}/>
-              <Route path="Settings" element={<Settings user={user} />}>
-                <Route path=":uid" element={<Settings/>}/>
-              </Route>
-              <Route path="Profil" element={<Profil/>}>
+              <Route path="Profil" element={<Profil user={user} />}>
+                <Route path=":uid" element={<Profil/>}>
+                </Route>
                 
               </Route>
+              <Route path="Update" element={<EditUser  user={user}/>}>
+                    <Route path=":uid" element={<EditUser  user={user}/>}/>
+                  </Route>
             </Routes>
           </Col>
         </Row>
